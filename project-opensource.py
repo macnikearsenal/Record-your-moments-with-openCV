694da9c713fc8fa076a57b5a399df5efb52af99b
wwnjwchww <wwnjwchww@gmail.com>
2023-12-14 22:49:48 +0900

    worker()추가/10:49/byjiwon

--- project-opensource.py
+++ project-opensource.py
@@ -1,43 +1,32 @@
+
 import speech_recognition as sr
+import threading
 import pygame
 import pygame.camera
-
-
-
+import cv2
 
 
 global user_speak
 user_speak = "None"
 num = 1
 
+class Worker(threading.Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name            # thread 이름 지정
 
-num = 1
-while True:
-    print("준비중 ...")
-
-
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=5)
-        print("Say Something")
-        speech = r.listen(source)
-        try:
-            audio = r.recognize_google(speech, language="ko-KR")
-            user_speak = audio
-            print("Your speech thinks like\n " + audio)
-            if  "안녕" in user_speak:
-                pygame.camera.init()
-                cameras =pygame.camera.list_cameras()
-                cam = pygame.camera.Camera(cameras[0])
-                cam.start()
-                image = cam.get_image()
-                pygame.image.save(image, 'image/'+str(num)+'.jpg')
-                num = num +1
-                cam.stop()
-
-        except sr.UnknownValueError:
-            print("Your speech can not understand")
-        except sr.RequestError as e:
-            print("Request Error!; {0}".format(e))
-          
-
+    def run(self):
+        global user_speak
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source, duration=5)
+            print("Say Something")
+            speech = r.listen(source)
+            try:
+                audio = r.recognize_google(speech, language="ko-KR")
+                user_speak = audio
+                print("Your speech thinks like\n " + audio)
+            except sr.UnknownValueError:
+                print("Your speech can not understand")
+            except sr.RequestError as e:
+                print("Request Error!; {0}".format(e))
